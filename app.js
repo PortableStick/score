6db90d423579abb7254044e850f32821e17b8a66
PortableStick <crashingwaves@fea.st>
2017-05-26 17:31:58 -0600

    Fix config

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: SESSION_SECRET, saveUninitialized: true, resave: true, store: new RedisStore(redisClient) }))
+app.use(session({ secret: SESSION_SECRET, saveUninitialized: true, resave: true, store: new RedisStore({ client: redisClient }) }))
 app.use(grant)
 
 
